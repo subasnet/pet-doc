151fa47ab37623839a665c52ac2208ba93bb7b48
Subash Basnet <subash.basnet345@gmail.com>
2020-11-18 10:16:56 +0200

    Adds extra modules and imports them into index.js.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'jquery/dist/jquery';
+import 'popper.js/dist/popper';
+import 'bootstrap/dist/js/bootstrap';
 ReactDOM.render(
   <React.StrictMode>
     <App />
