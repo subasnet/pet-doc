90a2aca8ded0a63b9e557d9c4a9ab82f072cbe53
Subash Basnet <subash.basnet345@gmail.com>
2020-11-18 11:17:01 +0200

    Organizes files & folders and replacing default codes.

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import react, {Component} from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {/* <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments} /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+  );
+}
+
+export default App;
