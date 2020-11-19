18ab96bc33265a88f375f4bd6bf28d56179aee6c
Subash Basnet <subash.basnet345@gmail.com>
2020-11-19 15:14:59 +0200

    Adds feature to add and delete appointment.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
+import {without} from 'lodash';
 
 class App extends Component {
 
@@ -13,7 +14,36 @@ class App extends Component {
     this.state = {
       myAppointments: [],
       lastIndex: 0
-    }
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+  }
+
+  addAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex + 1
+    });
+
+  }
+
+  deleteAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    })
+  }
+
+  toggleForm(){
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
   }
 
   componentDidMount() {
@@ -41,9 +71,13 @@ class App extends Component {
             <div className="row">
               <div className="col-md-12 bg-white">
                 <div className="container">
-                  <AddAppointments />
+                  <AddAppointments  
+                    formDisplay={this.state.formDisplay}
+                    toggleForm={this.toggleForm}
+                    addAppointment={this.addAppointment}/>
                   <SearchAppointments />
-                  <ListAppointments appointments = {this.state.myAppointments} />
+                  <ListAppointments appointments = {this.state.myAppointments} 
+                    deleteAppointment = {this.deleteAppointment}/>
                 </div>
               </div>
             </div>
