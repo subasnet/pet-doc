b163bda287ad14ada5350697052546fcd0fe2dbd
Subash Basnet <subash.basnet345@gmail.com>
2020-11-19 11:29:30 +0200

    Adds sample data and shows it in the list.

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,55 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 bg-white">
-              <div className="container">
-                <AddAppointments />
-                <SearchAppointments />
-                <ListAppointments />
+class App extends Component {
+
+  constructor(){
+    super(); // super method allows to get info from parent component, 
+              //pass info from other component to this component,
+              //allows to use this keyword.
+    this.state = {
+      myAppointments: [],
+      lastIndex: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(Response => Response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({lastIndex: this.state.lastIndex + 1});
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+
+      });
+
+  }
+
+  render() {
+
+    return (
+      <main className="page bg-white" id="petratings">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 bg-white">
+                <div className="container">
+                  <AddAppointments />
+                  <SearchAppointments />
+                  <ListAppointments appointments = {this.state.myAppointments} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-  );
+        </main>
+    );
+  }
 }
+
+
 
 export default App;
